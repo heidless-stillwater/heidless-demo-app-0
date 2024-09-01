aa7a7f675ada1ac85e093a217914a5fc9649682a
rob craig <heidlessemail08@gmail.com>
2024-09-01 15:30:42 +0100

    views added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_01_115206) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_01_141905) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_01_115206) do
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "views", default: 0
   end
 
   create_table "users", force: :cascade do |t|
