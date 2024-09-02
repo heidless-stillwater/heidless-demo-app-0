22ef5002b98f6e912836f4b6f5828075fc8707be
rob craig <heidlessemail08@gmail.com>
2024-09-02 10:11:38 +0100

    tweak

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: {minimum: 5, maximum: 50}
-  validates :content, presence: true, length: {minimum: 10, maximum: 1000}
+  validates :title, presence: true, length: {minimum: 2}
+  validates :content, presence: true, length: {in: 5..100}
+  validates :views, numericality: {only_integer: true}
 
   belongs_to :user
 end
